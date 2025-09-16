2a8fc5cf1725ab49b2f4af58e64b9228387c825f
Dmitri <dmitri.zgurski@iis.utm.md>
2025-09-16 15:40:48 +0300

    Sizes Entity

---
+++ app/(dahboard)/[storeId]/(routes)/categories/components/client.tsx
@@ -0,0 +1,46 @@
+"use client"
+
+import { Hand, Plus } from "lucide-react"
+import { useRouter, useParams } from "next/navigation"
+
+import { Billboard } from "@prisma/client"
+import { DataTable } from "@/components/ui/data-table"
+import { Heading } from "@/components/ui/heading"
+import { Button } from "@/components/ui/button"
+import { Separator } from "@radix-ui/react-separator"
+import { use } from "react"
+import { ApiList } from "@/components/ui/api-list"
+
+import { CategoryColumn, columns } from "./columns"
+
+interface CategoryClientProps {
+    data: CategoryColumn[]
+}
+
+export const CategoryClient: React.FC<CategoryClientProps> =  ({
+    data 
+  }) => {    
+    const route = useRouter();
+    const params = useParams();
+
+    return(
+        <>
+        <div className="flex items-center justify-between">
+            <Heading 
+                title={`Categories (${data.length})`}
+                description="Manage categories for your store"
+
+              />
+           <Button onClick={() => route.push(`/${params.storeId}/categories/new`)}>
+            <Plus className="mr-2 h-4 w-4" />
+            Add New
+           </Button>    
+        </div>
+        <Separator />
+        <DataTable searchKey="name" columns={columns} data={data} />
+        <Heading title="API" description="API calls for Categories " />
+        <Separator />
+        <ApiList entityName="categories" entityIdName="categories" />
+        </>
+    )
+}
